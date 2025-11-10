9b469792f0ce1610e602bfb477546bf8c5cc8706
Nano-DevCode <mayka708.ms@gmail.com>
2025-11-10 00:29:45 -0600

    changes

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -3,7 +3,7 @@ import {Manager, Socket} from 'socket.io-client';
 let socket: Socket;
 
 export const connectToServer = (token: string) => {
-    const manager = new Manager('https://teslobackend-lmd4.onrender.com//socket.io/socket.io.js',{
+    const manager = new Manager('https://teslobackend-lmd4.onrender.com/socket.io/socket.io.js',{
         extraHeaders: {
             hola: 'mundo', 
             authentication: token
